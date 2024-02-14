a22744f1157e6b0f15bcf1da84f3ae9ad830f014
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2024-02-14 22:53:10 +0530

    user auth satrts

---
+++ lib/Utils/user_auth.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserAuth{
+  static Future<User?> loginUsingEmailPassword({
+        required String email,
+        required String password,
+        required BuildContext context}) async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user
+
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:weather_assignment/login_page/login_page.dart';
+import 'package:weather_assignment/firebase_options.dart';
+import 'package:weather_assignment/login_page/login_page_widget.dart';
+import 'package:weather_assignment/network_services/firebase_app_initialize.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+  Future<FirebaseApp> _initializeFirebase() async{
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +35,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage(),
+      home: FutureBuilder(
+          future: _initializeFirebase(),
+          builder: (context, snapshot){
+            if(snapshot.connectionState == ConnectionState.done) {
+              return LoginPage();
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+      ),
     );
   }
 }
