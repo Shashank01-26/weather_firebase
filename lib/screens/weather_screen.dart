9be8a66cd44bfed0c0f06457b08ce6b0de8836c4
Shashank Kumar Srivastava <srivastavashashank033@gmail.com>
2024-02-16 19:45:00 +0530

    UI changes

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -23,137 +23,139 @@ class _WeatherScreenState extends State<WeatherScreen> {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Colors.black87, Colors.deepPurple, Colors.black87],
+            colors: [Colors.blueAccent, Colors.deepPurple, Colors.black87],
           ),
         ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 80,
-              left: 20,
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Center(
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  IconButton(
-                      onPressed: (){},
-                      icon: const Icon(Icons.favorite)
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(top:10.0),
+                    child: const Text(
+                      '15 Feb, 2024',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        '15 Feb, 2024',
-                        style: TextStyle(color: Colors.white, fontSize: 16),
+                  const SizedBox(height: 5),
+                  Text(
+                    _cityName,
+                    style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(80),
+                        gradient: LinearGradient(
+                          begin : Alignment.topLeft,
+                            end: Alignment.bottomLeft,
+                            colors: [Colors.indigo, Colors.blueGrey,Colors.teal]
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.45),
+                            blurRadius: 10,
+                            spreadRadius: 2,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 5),
-                      Text(
-                        _cityName,
-                        style: const TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                               Icon(Icons.wb_sunny, size: 100, color: Colors.yellow),
+                              IconButton(
+                                onPressed: (){},
+                                icon: Icon(
+                                  Icons.favorite_border_outlined
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          const Text(
+                            '25°C',
+                            style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            _weatherCondition,
+                            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.normal, color: Colors.white),
+                          ),
+                          const SizedBox(height: 80),
+                          Container(
+                            height: MediaQuery.sizeOf(context).height * 0.20,
+                            padding: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [Colors.purple.withOpacity(0.12), Colors.black.withOpacity(0.32),Colors.indigo.withOpacity(0.2)],
+                              ),
+                              borderRadius: BorderRadius.circular(50),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                _buildWeatherDetail('Wind', '10 km/h', Icons.wind_power),
+                                const VerticalDivider(color: Colors.white),
+                                _buildWeatherDetail('Humidity', '70%', Icons.water_drop),
+                                const VerticalDivider(color: Colors.white),
+                                _buildWeatherDetail('Precipitation', '20%', Icons.grain),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.blue, Colors.deepPurple],
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: BottomNavigationBar(
+                      currentIndex: _currentIndex,
+                      onTap: (index) {
+                        setState(() {
+                          _currentIndex = index;
+                          _onTabTapped(index);
+                        });
+                      },
+                      items: const [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: 'Home',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.search),
+                          label: 'Search',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.favorite),
+                          label: 'Favorites',
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(10),
-                    margin: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.blueGrey.withOpacity(0.8), // Adjusted card background color
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(Icons.wb_sunny, size: 100, color: Colors.yellow),
-                        const SizedBox(height: 20),
-                        const Text(
-                          '25°C',
-                          style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          _weatherCondition,
-                          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                        const SizedBox(height: 30),
-                        Container(
-                          height: MediaQuery.sizeOf(context).height * 0.20,
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [Colors.black.withOpacity(0.3), Colors.transparent],
-                            ),
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              _buildWeatherDetail('Wind', '10 km/h', Icons.wind_power),
-                              const VerticalDivider(color: Colors.white),
-                              _buildWeatherDetail('Humidity', '70%', Icons.water_drop),
-                              const VerticalDivider(color: Colors.white),
-                              _buildWeatherDetail('Precipitation', '20%', Icons.grain),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.blue, Colors.deepPurple],
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: BottomNavigationBar(
-                    currentIndex: _currentIndex,
-                    onTap: (index) {
-                      setState(() {
-                        _currentIndex = index;
-                        _onTabTapped(index);
-                      });
-                    },
-                    items: const [
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.home),
-                        label: 'Home',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.search),
-                        label: 'Search',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: Icon(Icons.favorite),
-                        label: 'Favorites',
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -161,6 +163,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
 
   Widget _buildWeatherDetail(String title, String value, IconData icon) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Icon(icon, color: Colors.white),
         const SizedBox(height: 10),
